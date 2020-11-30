e84c79ea6d7e18194583e75424986791744411c7
gpachecos <guilherme.pacheco30@gmail.com>
2020-11-30 01:48:55 -0300

    criamos a classe Page

--- index.php
+++ index.php
@@ -2,17 +2,18 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Nutri\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new Nutri\DB\Sql();
+	$page = new Page();
 
-	$results = $sql->select("SELECT * FROM categoriaalimentos");
-
-	echo json_encode($results);
+	$page->setTpl("index");
 
 });
 
